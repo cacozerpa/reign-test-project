7ca02bdc76a94d92112fdd9e7b9910b79b768468
Carlos Zerpa <zerpa2007@hotmail.com>
2021-10-26 18:07:00 -0400

    Created news entity

---
+++ src/news/entities/news.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class News {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  author: string;
+
+  @Column("simple-array")
+  _tags: string[];
+
+  @Column()
+  created_at: Date;
+}
